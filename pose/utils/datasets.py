2de757c5d62712db35251dc5b6faca7cefcac418
ziho9593 <no1hjyo@naver.com>
2023-08-01 15:57:35 +0900

    [#7] LoadImages 코드 정리

--- pose/utils/datasets.py
+++ pose/utils/datasets.py
@@ -123,35 +123,12 @@ class _RepeatSampler(object):
 
 class LoadImages:  # for inference
     def __init__(self, path, img_size=640, stride=32):
-        # p = str(Path(path).absolute())  # os-agnostic absolute path
-        # if '*' in p:
-        #     files = sorted(glob.glob(p, recursive=True))  # glob
-        # elif os.path.isdir(p):
-        #     files = sorted(glob.glob(os.path.join(p, '*.*')))  # dir
-        # elif os.path.isfile(p):
-        #     files = [p]  # files
-        # else:
-        #     raise Exception(f'ERROR: {p} does not exist')
-
-        # images = [x for x in files if x.split('.')[-1].lower() in img_formats]
-        # videos = [x for x in files if x.split('.')[-1].lower() in vid_formats]
-        # ni, nv = len(images), len(videos)
-
         self.img = path
         self.img_size = img_size
         self.stride = stride
-        # self.files = images + videos
-        # self.nf = ni + nv  # number of files
         self.nf = 1
-        # self.video_flag = [False] * ni + [True] * nv
         self.mode = 'image'
-        # if any(videos):
-        #     self.new_video(videos[0])  # new video
-        # else:
-        #     self.cap = None
-        # assert self.nf > 0, f'No images or videos found in {p}. ' \
-        #                     f'Supported formats are:\nimages: {img_formats}\nvideos: {vid_formats}'
-
+        
     def __iter__(self):
         self.count = 0
         return self
@@ -159,33 +136,9 @@ class LoadImages:  # for inference
     def __next__(self):
         if self.count == self.nf:
             raise StopIteration
-        # path = self.files[self.count]
-
-        # if self.video_flag[self.count]:
-        #     # Read video
-        #     self.mode = 'video'
-        #     ret_val, img0 = self.cap.read()
-        #     if not ret_val:
-        #         self.count += 1
-        #         self.cap.release()
-        #         if self.count == self.nf:  # last video
-        #             raise StopIteration
-        #         else:
-        #             path = self.files[self.count]
-        #             self.new_video(path)
-        #             ret_val, img0 = self.cap.read()
-
-        #     self.frame += 1
-        #     print(f'video {self.count + 1}/{self.nf} ({self.frame}/{self.nframes}) {path}: ', end='')
-
-        # else:
-        # Read image
         self.count += 1
-        # img0 = cv2.imread(path)  # BGR
-        img0 = self.img  # BGR
-        # assert img0 is not None, 'Image Not Found ' + path
-        # print(f'image {self.count}/{self.nf} {path}: ', end='')
 
+        img0 = self.img  # BGR
         # Padded resize
         img = letterbox(img0, self.img_size, stride=self.stride, auto=False)[0]
 
